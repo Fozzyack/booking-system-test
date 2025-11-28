45d96376156cc0c5913382a2491c5396e3135bd5
Frasier <fsundra@gmail.com>
2025-11-28 22:51:19 +0800

    resolves image issues

--- server/rooms/models.py
+++ server/rooms/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Rooms(models.Model):
     name = models.CharField(max_length=30)
     description = models.CharField(max_length=100)
-    image = models.ImageField(upload_to="rooms-images", blank=True, null=True)
+    image = models.ImageField(upload_to="", blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     
--- server/server/settings.py
+++ server/server/settings.py
@@ -125,6 +125,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+# Media files (uploaded images)
+MEDIA_URL = '/rooms-images/'
+MEDIA_ROOT = BASE_DIR / 'rooms-images'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
