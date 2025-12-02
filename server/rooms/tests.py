f7fb3a5191669c7cce2ac6243a6b032f90f1ea2f
Frasier <fsundra@gmail.com>
2025-12-02 15:16:12 +0800

    Adds: Simple tests for rooms

--- server/rooms/tests.py
+++ server/rooms/tests.py
@@ -17,3 +17,14 @@ class RoomsModelTest(TestCase):
         self.assertEqual(self.room.name, "Test Room")
         self.assertEqual(self.room.capacity, 10)
         self.assertEqual(self.room.good_for, "meetings")
+
+    def test_return_string_for_room(self):
+        self.assertEqual(str(self.room), "Test Room")
+
+    def test_room_type_relationship(self):
+        """Test one-to-one relationship with RoomType"""
+        room_type = RoomType.objects.create(
+            type="Conference",
+            room=self.room
+        )
+        self.assertEqual(self.room.room_type.type, "Conference")
