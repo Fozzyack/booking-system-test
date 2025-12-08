86c30d09df6290efce51e83b55d5a4c9593c8de1
Frasier <fsundra@gmail.com>
2025-12-08 18:20:37 +0800

    Adds created at field to user

--- server/user_auth/models.py
+++ server/user_auth/models.py
@@ -4,4 +4,5 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 class CustomUser(AbstractUser):
     email = models.EmailField(unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
