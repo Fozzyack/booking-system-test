52ddc9ab5fd2708bb0faa39dda1af88c09418b19
Frasier <fsundra@gmail.com>
2026-01-10 14:32:22 +0800

    Changes: Only renders time select component when date is selected

--- client/components/BookingSection.tsx
+++ client/components/BookingSection.tsx
@@ -11,12 +11,14 @@ const BookingSection = () => {
     return (
         <div className="px-4 md:p-0 space-y-8 sticky left-0 top-24">
             <BookingDateFilter currentDate={currentDate} selectedDate={selectedDate} setCurrentDate={setCurrentDate} setSelectedDate={setSelectedDate}/>
+            { selectedDate !== null &&
             <BookingTimeComponent
                 selectedTime={selectedTime}
                 setSelectedTime={setSelectedTime}
                 startTime_24hour={8}
                 endTime_24hour={19}
             />
+            }
         </div>
     );
 };
